68599d35976dfb8a60e7da7237b53ceb66524d19
dheera-01 <76260990+dheera-01@users.noreply.github.com>
2023-01-22 10:46:30 +0530

    Initialisation of project.

---
+++ sim.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+from fractions import Fraction
+from typing import Union, Mapping
+
+@dataclass
+class NumLiteral:
+    value: Fraction
+    def __init__(self, *args):
+        self.value = Fraction(*args)
+
+@dataclass
+class BinOp:
+    operator: str
+    left: 'AST'
+    right: 'AST'
+
+@dataclass
+class Variable:
+    name: str
+
+@dataclass
+class Let:
+    var: 'AST'
+    e1: 'AST'
+    e2: 'AST'
+
+AST = NumLiteral | BinOp | Variable | Let
+
+Value = Fraction
+
+class InvalidProgram(Exception):
+    pass
+
+def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
+    if environment is None:
+        environment = {}
+    match program:
+        case NumLiteral(value):
+            return value
+        case Variable(name):
+            if name in environment:
+                return environment[name]
+            raise InvalidProgram()
+        case Let(Variable(name), e1, e2):
+            v1 = eval(e1, environment)
+            return eval(e2, environment | { name: v1 })
+        case BinOp("+", left, right):
+            return eval(left, environment) + eval(right, environment)
+        case BinOp("-", left, right):
+            return eval(left, environment) - eval(right, environment)
+        case BinOp("*", left, right):
+            return eval(left, environment) * eval(right, environment)
+        case BinOp("/", left, right):
+            return eval(left, environment) / eval(right, environment)
+    raise InvalidProgram()
+
+def test_eval():
+    e1 = NumLiteral(2)
+    e2 = NumLiteral(7)
+    e3 = NumLiteral(9)
+    e4 = NumLiteral(5)
+    e5 = BinOp("+", e2, e3)
+    e6 = BinOp("/", e5, e4)
+    e7 = BinOp("*", e1, e6)
+    assert eval(e7) == Fraction(32, 5)
+
+def test_let_eval():
+    a  = Variable("a")
+    e1 = NumLiteral(5)
+    e2 = BinOp("+", a, a)
+    e  = Let(a, e1, e2)
+    assert eval(e) == 10
+    e  = Let(a, e1, Let(a, e2, e2))
+    assert eval(e) == 20
+    e  = Let(a, e1, BinOp("+", a, Let(a, e2, e2)))
+    assert eval(e) == 25
+    e  = Let(a, e1, BinOp("+", Let(a, e2, e2), a))
+    assert eval(e) == 25
+    e3 = NumLiteral(6)
+    e  = BinOp("+", Let(a, e1, e2), Let(a, e3, e2))
+    assert eval(e) == 22
