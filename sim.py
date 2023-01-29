b9ed86bea53e83f4d12686148888b86d7d0f381b
RahulRai02 <rahulr0211@gmail.com>
2023-01-29 12:55:17 +0530

    Print Implemented

--- sim.py
+++ sim.py
@@ -9,6 +9,10 @@ class NumLiteral:
         self.value = Fraction(*args)
 
 @dataclass
+class StringLiteral:
+    value: str
+
+@dataclass
 class BinOp:
     operator: str
     left: 'AST'
@@ -24,9 +28,13 @@ class Let:
     e1: 'AST'
     e2: 'AST'
 
-AST = NumLiteral | BinOp | Variable | Let
+@dataclass
+class Print:
+    value: 'AST'
 
-Value = Fraction
+AST = NumLiteral | BinOp | Variable | Let | Print | StringLiteral
+
+Value = Fraction | str
 
 class InvalidProgram(Exception):
     pass
@@ -37,10 +45,15 @@ def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
     match program:
         case NumLiteral(value):
             return value
+        case StringLiteral(value):
+            return value
         case Variable(name):
             if name in environment:
                 return environment[name]
             raise InvalidProgram()
+        case Print(value):
+            print(eval(value))
+            return eval(value)
         case Let(Variable(name), e1, e2):
             v1 = eval(e1, environment)
             return eval(e2, environment | { name: v1 })
@@ -79,3 +92,20 @@ def test_let_eval():
     e3 = NumLiteral(6)
     e  = BinOp("+", Let(a, e1, e2), Let(a, e3, e2))
     assert eval(e) == 22
+
+def test_print():
+    # Check on number literal
+    e1 = NumLiteral(2)
+    e2 = NumLiteral(5)
+    assert eval(Print(e2)) == Fraction(5, 1)
+    assert eval(Print(e1)) == Fraction(2, 1)
+
+    # Check on string literal
+    e3 = StringLiteral("Hello")
+    assert eval(Print(e3)) == "Hello"
+
+    # Check on binop
+    e4 = BinOp("+", e1, e2)
+    assert eval(Print(e4)) == Fraction(7, 1)
+    
+
