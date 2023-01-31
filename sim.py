1e63f85cb2a3fcda177ea82077ddc5b96ec17f92
RahulRai02 <rahulr0211@gmail.com>
2023-01-31 12:57:09 +0530

    Print upd + Assignment added

--- sim.py
+++ sim.py
@@ -8,6 +8,7 @@ class NumLiteral:
     def __init__(self, *args):
         self.value = Fraction(*args)
 
+# Define a stringLiteral class for testing in test_print
 @dataclass
 class StringLiteral:
     value: str
@@ -32,12 +33,26 @@ class Let:
 class Print:
     value: 'AST'
 
-AST = NumLiteral | BinOp | Variable | Let | Print | StringLiteral
+@dataclass
+class Assignment:
+    operator: str
+    left: 'AST'
+    right: 'AST'
+
+AST = NumLiteral | BinOp | Variable | Let | Print | StringLiteral | Assignment
 
 Value = Fraction | str
 
 class InvalidProgram(Exception):
     pass
+
+def PrintHelper(value):
+    if isinstance(value, NumLiteral) or isinstance(value, StringLiteral) or isinstance(value, BinOp):
+        print(eval(value))
+        return eval(value)
+    else:
+        raise InvalidProgram()
+
 
 def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
     if environment is None:
@@ -52,8 +67,11 @@ def eval(program: AST, environment: Mapping[str, Value] = None) -> Value:
                 return environment[name]
             raise InvalidProgram()
         case Print(value):
-            print(eval(value))
-            return eval(value)
+            return PrintHelper(value)
+        case Assignment("=", left, right):
+            right_val = eval(right)
+            environment[left.name] = right_val
+            return right_val
         case Let(Variable(name), e1, e2):
             v1 = eval(e1, environment)
             return eval(e2, environment | { name: v1 })
@@ -107,5 +125,20 @@ def test_print():
     # Check on binop
     e4 = BinOp("+", e1, e2)
     assert eval(Print(e4)) == Fraction(7, 1)
-    
 
+    
+def test_assignment():
+    a  = Variable("a")
+    e1 = NumLiteral(2)
+    e2 = Assignment("=", a, e1)
+
+    assert eval(e2) == Fraction(2, 1)
+
+    b = Variable("b")      
+    e3 = NumLiteral(5)      
+    e4 = Assignment("=", b, e3)     #b = 5
+    assert eval(e4) == Fraction(5, 1)
+    
+    # Reassignment 
+    e6 = Assignment("=", a, e3)
+    assert eval(e6) == Fraction(5, 1)
