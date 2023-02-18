c6b0733a2b357884b593b14da748d94494463ae9
dheera-01 <76260990+dheera-01@users.noreply.github.com>
2023-02-18 09:31:04 +0530

    Initial.

--- samples/if_else_types.py
+++
@@ -1,3 +0,0 @@
-x: int|str = 3 if 2 > 1 else "Hello"
-# y: int = 3 if 2 > 1 else "Hello"
-z: int = 3 if 2 > 1 else 5
--- samples/type_checking.py
+++
@@ -1,6 +0,0 @@
-# Running Python will launch missiles and error.
-# Running mypy will detect the error without launching missiles.
-a = 0
-print("Launch Missiles.")
-b = "xyz" + a
-print(b)
