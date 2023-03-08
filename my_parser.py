96518f7ed3cf6ba00691f058dd85c9311d666f70
Chirag Sarda <chirag.sarda@iitgn.ac.in>
2023-03-09 01:28:36 +0530

    parser updated to fit update expression without assign

---
+++ my_parser.py
@@ -0,0 +1,421 @@
+from my_lexer import *
+from dataclasses import dataclass
+import sys
+from declaration import *
+from eval_for_parser import *
+from sim import *
+
+@dataclass
+class Parser:
+    lexer: Lexer
+    mySequence = Sequence([])
+
+    def __init__(self, _lexer):
+        self.lexer = _lexer
+        self.mySequence = Sequence([])
+
+    def from_lexer(_lexer):
+        """give a tokens (lexer output ) to the parser
+
+        Args:
+            Lexer: lexer output
+
+        Returns:
+            Parser: parser object
+        """
+        return Parser(_lexer)
+
+    def parse_if(self):
+        """parse if else statement
+
+        Returns:
+            IfElse: return AST of if else statement
+        """
+        self.lexer.match(Keyword("if"))
+        c = self.parse_simple()  # parse the condition which is a simple expression
+        if_branch = self.parse_block()
+        # single if statement
+        if self.lexer.peek_current_token() != Keyword("else"):
+            return IfElse(c, if_branch)
+        self.lexer.match(Keyword("else"))
+        else_branch = self.parse_block()
+
+        return IfElse(c, if_branch, else_branch)
+
+
+    def parse_while(self):
+        """parse while statement
+
+        Returns:
+            while AST: return AST of while loop
+        """
+        self.lexer.match(Keyword("while"))
+        # print(self.lexer.peek_current_token())
+        cond = self.parse_simple()  # parse the condition
+        # print("cond", cond)
+        while_body = self.parse_block()
+        return While(cond, while_body)
+
+    def parse_for(self):
+        """parse for statement
+
+        Returns:
+            For: return AST of for loop
+        """
+        self.lexer.match(Keyword("for"))
+        self.lexer.match(Bracket("("))
+        initial = self.parse_expr()
+        cond = self.parse_simple()
+        self.lexer.match(EndOfLine(";"))
+        termination = self.parse_expr()
+        self.lexer.match(Bracket(")"))
+        for_body = self.parse_block()
+        # print(self.lexer.peek_current_token())
+        return For(initial, cond, termination, for_body)
+
+    def parse_atom(self):
+        """parse the atomic expression"""
+        match self.lexer.peek_current_token():
+
+            case Identifier(name):
+                self.lexer.advance()
+                return Identifier(name)
+            case StringLiteral(value):
+                self.lexer.advance()
+                return StringLiteral(value)
+            case NumLiteral(value):
+                self.lexer.advance()
+                return NumLiteral(value)
+            case FloatLiteral(value):
+                self.lexer.advance()
+                return FloatLiteral(value)
+            case BoolLiteral(value):
+                self.lexer.advance()
+                return BoolLiteral(value)
+            case Keyword("slice"):
+                return self.parse_slice()
+            case Keyword("let"):
+                return self.parse_let()
+            case Bracket("(") | Bracket("[") | Bracket("{"):
+                self.lexer.advance()  # consume the opening bracket
+                ans = self.parse_simple()  # calculating the expression inside the brackets
+                self.lexer.advance()  # consume the closing bracket
+                return ans
+
+    def parse_exponent(self):
+        """parse the exponent operator, this is right associative
+
+        Returns:
+            AST: return AST of the exponent operation
+        """
+
+        temp = []
+        left = self.parse_atom()
+        temp.append(left)
+        while True:
+            match self.lexer.peek_current_token():
+                case Operator(op) if op == "**":
+                    self.lexer.advance()
+                    m = self.parse_atom()
+                    temp.append(m)
+                case _:
+                    break
+
+        # making the exponent right associative
+        left = temp.pop()
+        while len(temp) > 0:
+            left = BinOp(temp.pop(), "**", left)
+        return left
+
+    def parse_unary(self):
+        """parse the unary operator, this is left associative
+
+        Returns:
+            AST: return AST of the unary operation
+        """
+
+        # unary operator is left associative
+        left = self.parse_exponent()
+        if left == None:
+            while True:
+                # print(f"current token {self.lexer.peek_current_token()}")
+                match self.lexer.peek_current_token():
+                    case Operator(op) if op in "-+":
+                        self.lexer.advance()
+                        exp = self.parse_exponent()
+                        if(exp != None):
+                            return UnaryOp(op, exp)
+                        else:
+                            return UnaryOp(op, self.parse_unary())
+                    case _:
+                        break
+        return left
+
+    def parse_mult(self):
+        """parse the *, /, //, % operator
+
+        Returns:
+            AST: return AST of the *, /, //, % operation
+        """
+
+        left = self.parse_unary()
+
+        while True:
+            match self.lexer.peek_current_token():
+
+                case Operator(op) if op in "* / // %".split():
+                    self.lexer.advance()
+                    m = self.parse_unary()
+                    left = BinOp(left, op, m)
+
+                case _:
+                    break
+
+        return left
+
+    def parse_add(self):
+        """parse the addition and subtraction operator
+
+        Returns:
+            AST: return AST of the addition and subtraction operation
+        """
+        left = self.parse_mult()
+
+        while True:
+            match self.lexer.peek_current_token():
+
+                case Operator(op) if op in "+-":
+                    self.lexer.advance()
+                    # print("before parse_add")
+                    m = self.parse_mult()
+                    # print("after parse_add",m)
+                    left = BinOp(left, op, m)
+
+                case _:
+                    break
+
+        return left
+
+    def parse_slice(self):
+        """parse the slice operation
+
+        Returns:
+            AST: return AST of the slice operation
+        """
+        self.lexer.match(Keyword("slice"))
+        self.lexer.match(Bracket("("))
+        string_literal = self.parse_simple()
+        self.lexer.match(Operator(":"))
+        start = self.parse_simple()
+        self.lexer.match(Operator(":"))
+        end = self.parse_simple()
+        self.lexer.match(Operator(":"))
+        step = self.parse_simple()
+        self.lexer.match(Bracket(")"))
+        return Slice(string_literal, start, end, step)
+
+    def parse_cmp(self):
+        """parse the comparison operator
+
+        Returns:
+            AST: return AST of the comparison operator
+        """
+
+        left = self.parse_add()
+        match self.lexer.peek_current_token():
+
+            case Operator(op) if op in "< > >= <= == !=".split():
+                self.lexer.advance()
+                right = self.parse_add()
+
+                return ComparisonOp(left, op, right)
+
+        return left
+
+    def parse_simple(self):
+        """parse the simple expression (without if else, while, for, assign, const and update, or something returning none) 
+
+        Returns:
+            AST: return AST of the simple expression
+        """
+
+        return self.parse_cmp()
+    
+    def parse_assign(self):
+        """
+        parse the assign expression
+        
+        Returns:
+            Assign: return AST of the assign expression
+        """
+        self.lexer.match(Keyword("assign"))  # consume the assign keyword
+        left_part = self.parse_atom()
+        self.lexer.match(Operator("="))
+        right_part = self.parse_simple()
+        self.lexer.match(EndOfLine(";"))
+        return Assign(left_part, right_part)
+    
+
+    def parse_const(self):
+        """paster the immutable assign expression
+
+        Returns:
+            Assign: return AST of the immutable assign expression
+        """
+        self.lexer.match(Keyword("const"))
+        self.lexer.match(Keyword("assign"))
+        left_part = self.parse_atom()
+        left_part.is_mutable = False
+        self.lexer.match(Operator("="))
+        right_part = self.parse_simple()
+        self.lexer.match(EndOfLine(";"))
+        return Assign(left_part, right_part)
+
+    def parse_update(self):
+        """parse the update expression
+
+        Returns:
+            Update: return AST of the update expression
+        """
+        left_part = self.parse_atom()
+        
+        assignment_operator_list = "= -= += *= /= %= //= **=".split()
+        op = self.lexer.peek_current_token()
+        if not isinstance(op, Operator):
+            raise InvalidProgram(f"Syntax Error: Expected an assignment operator but got {op}")
+        if op._operator not in assignment_operator_list:
+            raise InvalidProgram(f"Syntax Error: {op} not a valid assignment operator")
+        self.lexer.advance() # consuming the assignment operator
+        
+        right_part = self.parse_simple()
+        self.lexer.match(EndOfLine(";"))
+        return Update(left_part, op, right_part)
+    
+
+    def parse_print(self):
+        """parse the print expression
+
+        Returns:
+            Print: return AST of the print expression
+        """
+        self.lexer.match(Keyword("print"))
+        self.lexer.match(Bracket("("))
+        print_statement = self.parse_simple()
+        self.lexer.match(Bracket(")"))
+        self.lexer.match(EndOfLine(';'))
+        return Print(print_statement)
+
+    def parse_let(self):
+        """parse the let expression
+        Returns:
+            returns the value of expression after in keyword
+        """
+
+        self.lexer.match(Keyword("let"))
+        left_part = self.parse_atom()
+        self.lexer.match(Operator("="))
+        right_part = self.parse_simple()
+        self.lexer.match(Bracket("("))
+        body = self.parse_simple()
+        self.lexer.match(Bracket(")"))
+        return Let(Assign(left_part, right_part), body)
+
+        # return Let(Assign(left_part,right_part), self.parse_expr())
+
+
+    def parse_expr(self):
+        """parse the expression
+
+        Returns:
+            AST: return AST of the expression
+        """
+        match self.lexer.peek_current_token():
+
+            case c if isinstance(c, EndOfLine):
+                self.lexer.advance()
+                return EndOfLine(";")
+            case c if isinstance(c, EndOfFile):
+                return EndOfFile("EOF")
+                sys.exit(0)
+            case Keyword("if"):
+                # print(self.lexer.peek_current_token())
+                return self.parse_if()
+            case Keyword("while"):
+                return self.parse_while()
+            case Keyword("for"):
+                return self.parse_for()
+            case Keyword("assign"):
+                return self.parse_assign()
+            case Keyword("const"):
+                return self.parse_const()
+            # update statements
+            case c if isinstance(c, Identifier):
+                return self.parse_update()          
+            case Keyword("print"):
+                return self.parse_print()
+
+            case _:
+                return self.parse_simple()
+
+    # statements with {} is considered blocks
+    def parse_block(self) -> Sequence:
+        """parse the block. block starts with Brackets('{')
+
+        Returns:
+            Sequence: return AST of the block
+        """
+        self.lexer.match(Bracket("{"))
+        block_sequence = Sequence([])
+        while True:
+            t = self.parse_expr()
+            # } will not be parsed as it is not a valid expression 
+            if t == None and self.lexer.peek_current_token() == Bracket("}"):
+                break
+            block_sequence.statements.append(t)
+        self.lexer.match(Bracket("}"))
+        return block_sequence
+
+    def parse_program(self) -> Sequence:
+        """parse the program
+
+        Returns:
+            Sequence: return AST of the program
+        """
+        # print(f"parse program function: {self}")
+        while True:
+            t = self.parse_expr()
+            if (t == EndOfFile("EOF")):
+                break
+            self.mySequence.statements.append(t)
+        return self.mySequence
+    
+    def __repr__(self) -> str:
+        return f"Parser:\nLexer: {self.lexer}\nSequence: {self.mySequence}"
+
+
+def parse_code_file(file_location:str):
+    '''
+    to parse and evaluate given file present file_Location
+    '''
+
+    file = open(file_location, "r")
+
+    program = file.read()
+    obj_parser = Parser.from_lexer(
+        Lexer.from_stream(Stream.from_string(program)))
+
+    a = obj_parser.parse_program()
+    print(a)
+    program_env = Environment()
+    print(program_env)
+    ans = eval(a, program_env)
+
+if __name__ == '__main__':
+
+    file = open("program.txt", "r")
+    program = file.read()
+    obj_parser = Parser.from_lexer(
+        Lexer.from_stream(Stream.from_string(program)))
+    print(f"object parser {obj_parser}")
+    a = obj_parser.parse_program()
+    print(f"Parsed program: {a}")
---
+++ testcase/testcase_parse.py
@@ -0,0 +1,158 @@
+import os, sys
+current_dir = os.getcwd()
+sys.path.append(current_dir)
+# print(f"syst path {sys.path}")
+
+from my_parser import *
+
+# arithmetic, unary operation, precedence and associativity
+def test_case1():
+    print("\nTest case 1")
+    text = """
+    # unary operator
+    ++--6 
+    i = -2**6**8; # exponent have high precedence
+    # simple expression
+    8 / 6
+    # precedence
+    8 + 9 * 10
+    2 * (3+5)
+    # right associativity
+    2 * 3 * 4
+    # right associativity
+    2 ** 3 ** 3
+    """
+    obj_parser = Parser.from_lexer(Lexer.from_stream(Stream.from_string(text)))
+    for i in obj_parser.parse_program().statements:
+        print(i)
+        
+
+# test case assign and update
+def test_case2():
+    print("\nTest case 2")
+    text = """
+    assign i = i+5;
+    const assign j = i+5;
+    j = j + 1 * i;
+    """
+    obj_parser = Parser.from_lexer(Lexer.from_stream(Stream.from_string(text)))
+    for i in obj_parser.parse_program().statements:
+        print(i)
+
+
+# if else test
+def test_case3():
+    print("\nTest case 3")
+    text = """
+    # if else both
+    if("hello world" > "chirag")
+    {
+        i = i + k +l;
+        i = 10;
+    }
+    else 
+    {
+        "hello"
+    }
+    
+    
+    # only if
+    if(i>5)
+    {
+        i = i +5;
+        8+5
+    }
+   
+    """
+    obj_parser = Parser.from_lexer(Lexer.from_stream(Stream.from_string(text)))
+    for i in obj_parser.parse_program().statements:
+        print(i)
+
+
+# for test
+def test_case4():
+    print("\nTest case 4")
+    text = """
+    assign  i = 7
+    ;
+    assign n = 10;
+    for(i = i + 1 ; i< n; i+=1;)
+    {
+        print(i>1);    
+    }
+    """
+    obj_parser = Parser.from_lexer(Lexer.from_stream(Stream.from_string(text)))
+    for i in obj_parser.parse_program().statements:
+        print(i)
+
+
+# while test
+def test_case5():
+    print(f"\nTest Case 5")
+    text = """
+    assign j = 0;
+    while(j<10)
+    {
+        j = j +1;
+        print(j);
+    }
+    """
+    obj_parser = Parser.from_lexer(Lexer.from_stream(Stream.from_string(text)))
+    a = obj_parser.parse_program()
+    for i in a.statements:
+        print(i)
+
+
+#string test
+def test_case6():
+    print(f"\nTest Case 6")
+    text = """
+        #1
+        assign a = "Hello World!";
+        print(a);
+
+        #2
+        a = "Hello World!";
+        a = slice(a: 0: 5: 1);
+        print(a);
+
+        #3
+        #slice("Hello World!": 0: 5: 1)
+    """
+    obj_parser = Parser.from_lexer(Lexer.from_stream(Stream.from_string(text)))
+    a = obj_parser.parse_program()
+    for i in a.statements:
+        print(i)
+
+# let test
+def test_case7():
+    print(f"\nTest Case 7")
+    text = """
+    let x=1+2*3 (x+1)
+    let y=2 (let x=2 (x+y));
+    print(x);
+    print("hello");
+    let x=2 (let y=3 (let z=4 (x+y+z)));
+    assign v=let u=2
+    (
+        let t=2
+            (let a=2(u+ a*t))
+    );
+    print(v);
+    """
+    obj_parser = Parser.from_lexer(Lexer.from_stream(Stream.from_string(text)))
+    a = obj_parser.parse_program()
+    for i in a.statements:
+        print(i)
+    
+if __name__ == "__main__":
+
+    # test_case1()
+    # test_case2()
+    # test_case3()
+    # test_case4()
+    # test_case5()
+    # test_case6()
+    test_case7()
+    print("\nAll test cases passed")
+    pass
