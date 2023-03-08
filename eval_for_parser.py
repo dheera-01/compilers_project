ef4fbd83a053990ee2d6809e7cf3ce982a9e6698
Chirag Sarda <chirag.sarda@iitgn.ac.in>
2023-03-09 01:29:35 +0530

    eval for update added

--- eval_for_parser.py
+++ eval_for_parser.py
@@ -2,38 +2,43 @@ from dataclasses import dataclass
 from typing import Union, Mapping
 from declaration import *
 
-Value_literal = int | float | bool | str
-Value = None | NumLiteral | StringLiteral | BoolLiteral | FloatLiteral
 
 global_env = {}
 
 
-def eval_literals(Literal: AST) -> Value_literal:
-    match Literal:
+def eval_literals(literal: Value) -> Value_literal:
+    match literal:
         case NumLiteral(value):
             return value
-
+        
         case FloatLiteral(value):
             return value
 
         case StringLiteral(value):
             return value
 
-        case BoolLiteral(Value):
-            return Value
+        case BoolLiteral(value):
+            return value
 
 
-def eval(program: AST, program_env:Enviroment, environment: Mapping[str, Value] = None) -> Value:
+def eval(program: AST, program_env:Environment, environment: Mapping[str, Value] = None) -> Value:
     global global_env
     if environment is None:
         environment = {}
 
     match program:
         case Sequence(statements):
-            ans = []
             for statement in statements:
-                ans.append(eval(statement, program_env, environment))
-            return ans
+                # print(f"statement: {statement}")
+                eval(statement, program_env ,environment)
+            return None
+            # print(f"ans: {ans}")
+            # ans = []
+            # for statement in statements:
+            #     # print(f"statement: {statement}")
+            #     ans.append(eval(statement, environment))
+            # # print(f"ans: {ans}")
+            # return ans
 
         case NumLiteral(value):
             return program
@@ -44,7 +49,7 @@ def eval(program: AST, program_env:Enviroment, environment: Mapping[str, Value] 
         case StringLiteral(value):
             return program
 
-        case BoolLiteral(Value):
+        case BoolLiteral(value):
             return program
 
         case Identifier(name):
@@ -62,53 +67,92 @@ def eval(program: AST, program_env:Enviroment, environment: Mapping[str, Value] 
 
             program_env.enter_scope()
             e2_val = eval(e2, program_env)
+            
             program_env.exit_scope()
             return e2_val
+        
+        case Assign(identifier, right):
+            value = eval(right, program_env, environment)
+            # for env in reversed(program_env.envs):
+            #     if identifier.name in env:
+            #         program_env.update(identifier, value)
+            #         return None
 
-        case Print(val):
+            program_env.add(identifier, value)
+            return None
+        case Update(identifier, op, right):
+            value = eval(right, program_env, environment)
+            if op._operator == "=":
+                program_env.update(identifier, value)
+            else:# op is +=, -=, *=, /=, %=, **= (binop of first to second last char)
+                v = eval(BinOp(identifier, op._operator[: len(op._operator) -1], right), program_env, environment)
+                program_env.update(identifier, v)
+            return None 
+        case Print(value):
             # The print function will print the evaluated value of val and return the AST val
-            if isinstance(val, NumLiteral) or isinstance(val, StringLiteral) or isinstance(val, BinOp) or isinstance(val, Identifier) or isinstance(val, BoolLiteral):
+            val = eval(value, program_env, environment)
+            # print(f"val: {val}")
+            if isinstance(val, NumLiteral) or isinstance(val, StringLiteral)  or isinstance(val, Identifier) or isinstance(val, BoolLiteral) or isinstance(val, FloatLiteral):
                 # print(f"----------------------------------------")
-                print(eval_literals(eval(val, program_env)))
+                ans = eval_literals(eval(val, program_env))
+                print(ans)
+                # print(eval_literals(eval(val, program_env)))
                 # print(f"----------------------------------------")
                 return None
             else:
-                raise InvalidProgram()
+                raise InvalidProgram(f"SyntaxError: invalid syntax for print")
 
-        case BoolLiteral(tf):
-            if tf == "True":
-                return BoolLiteral(True)
-            elif tf == "False":
-                return BoolLiteral(False)
-            raise InvalidProgram()
+        # case BoolLiteral(tf):
+        #     if tf == "True":
+        #         return BoolLiteral(True)
+        #     elif tf == "False":
+        #         return BoolLiteral(False)
+        #     raise InvalidProgram()
 
         case While(cond, body):
-            program_env.enter_scope()
             c = eval(cond, program_env, environment)
-            # if(c==True):
-            #     eval(body)
-            #     eval(While(cond,body))
-            body_iteration_lst = []
-            while (eval_literals(c) == True):
-                body_iteration_lst.append(eval(body, program_env, environment))
+            while (eval_literals(c) == True) :
+                program_env.enter_scope()
+                eval(body, program_env ,environment)
+                program_env.exit_scope()
                 c = eval(cond, program_env, environment)
-            # while loop cannot be implemented recursively as max recursion depth of python restricts it
-            program_env.exit_scope()
-            return body_iteration_lst
+            return None
+            
+            
+            # program_env.enter_scope()
+            # c = eval(cond, program_env, environment)
+            # # if(c==True):
+            # #     eval(body)
+            # #     eval(While(cond,body))
+            # body_iteration_lst = []
+            # while (eval_literals(c) == True):
+            #     body_iteration_lst.append(eval(body, program_env, environment))
+            #     c = eval(cond, program_env, environment)
+            # # while loop cannot be implemented recursively as max recursion depth of python restricts it
+            # program_env.exit_scope()
+            # return body_iteration_lst
 
         case For(exp1, condition, exp2, body):
             program_env.enter_scope()
-            eval(exp1, program_env, environment)
-            cond = eval(condition, program_env, environment)
-            body_iteration_lst = []
-            if (eval_literals(cond) == True):
-                temp = eval(body, program_env, environment)
-                temp.append(eval(exp2, program_env, environment))
-                body.statements.append(exp2)
-                body_iteration_lst = (eval(While(condition, body), program_env))
-                body_iteration_lst.insert(0, temp)
+            eval(exp1, program_env ,environment)
+            cond = eval(condition, program_env ,environment)
+            body.statements.append(exp2)
+            eval(While(condition, body), program_env ,environment)
             program_env.exit_scope()
-            return body_iteration_lst
+            return None
+            
+            # program_env.enter_scope()
+            # eval(exp1, program_env, environment)
+            # cond = eval(condition, program_env, environment)
+            # body_iteration_lst = []
+            # if (eval_literals(cond) == True):
+            #     temp = eval(body, program_env, environment)
+            #     temp.append(eval(exp2, program_env, environment))
+            #     body.statements.append(exp2)
+            #     body_iteration_lst = (eval(While(condition, body), program_env))
+            #     body_iteration_lst.insert(0, temp)
+            # program_env.exit_scope()
+            # return body_iteration_lst
 
         case Slice(string_var, start, end, step):
             string_var = eval(string_var, program_env, environment)
@@ -297,25 +341,18 @@ def eval(program: AST, program_env:Enviroment, environment: Mapping[str, Value] 
             eval_left = eval(left, program_env, environment)
             eval_right = eval(right, program_env, environment)
             try:
-                res = eval_literals(eval_left) % eval_literals(eval_right)
+                res = eval_literals(eval_left) ** eval_literals(eval_right)
                 return NumLiteral(res)
             except Exception as e:
                 raise InvalidProgram(
                     f"TypeError: ** not supported between instances of {left} and {right}")
 
-        case Seq(lst):
-            for expr in lst:
-                eval(expr, program_env, environment)
-            return None
+        # case Seq(lst):
+        #     for expr in lst:
+        #         eval(expr, program_env, environment)
+        #     return None
 
-        case Assign(identifier, right):
-            value = eval(right, program_env, environment)
-            for env in reversed(program_env.envs):
-                if identifier.name in env:
-                    program_env.update(identifier, value)
-                    return None
-
-            program_env.add(identifier, value)
-            return None
+        
+            pass
 
     raise InvalidProgram(f"SyntaxError: {program} invalid syntax")
